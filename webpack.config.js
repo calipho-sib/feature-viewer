cfb406af3c1660f0daf3d81196a02e9243347075
sachdeva-shrey <shreysachdeva.2000@gmail.com>
2021-06-02 19:38:01 +0530

    feat: bundle minified version
    Signed-off-by: sachdeva-shrey <shreysachdeva.2000@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
 const path = require("path");
-const webpack = require('webpack');
+const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TerserPlugin = require('terser-webpack-plugin');
+const TerserPlugin = require("terser-webpack-plugin");
+const { merge } = require("webpack-merge");
 
-module.exports = {
+const commonConfig = {
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: false,
+      }),
+    ],
+  },
+};
+
+// For minified version — without external dependencies
+const minConfig = {
+  entry: "./src/feature-viewer.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "",
+    library: "FeatureViewer",
+    libraryTarget: "var",
+    filename: "feature-viewer.min.js",
+  },
+};
+
+// For complete version — including external dependencies
+const maxConfig = {
   entry: "./lib/index.js",
 
   output: {
@@ -14,20 +39,14 @@ module.exports = {
     filename: "feature-viewer.bundle.js",
   },
 
-  optimization: {
-    minimizer: [new TerserPlugin({
-      extractComments: false,
-    })],
-  },
-
   plugins: [
     new HtmlWebpackPlugin({
-      template: './examples/simple.html'
+      template: "./examples/simple.html",
     }),
     new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-      d3: 'd3'
+      $: "jquery",
+      jQuery: "jquery",
+      d3: "d3",
     }),
   ],
 
@@ -35,11 +54,13 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"],
       },
-    ]
-  }
+    ],
+  },
 };
+
+module.exports = [
+  merge(commonConfig, minConfig),
+  merge(commonConfig, maxConfig),
+];
