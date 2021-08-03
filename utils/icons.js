0819e37153281c682c4d0881f9eb1728686d6f46
sachdeva-shrey <shreysachdeva.2000@gmail.com>
2021-08-04 01:13:50 +0530

    feat: add icons
    Signed-off-by: sachdeva-shrey <shreysachdeva.2000@gmail.com>

--- utils/icons.js
+++ utils/icons.js
@@ -1,3 +1,113 @@
-const crossIcon = `<path fill="#3a3a3a" d="M256,0h0C114.61,0,0,114.61,0,256H0C0,397.39,114.61,512,256,512h0c141.38,0,256-114.61,256-256h0C512,114.61,397.39,0,256,0Zm87.72,307c13.06,12.28,17.61,26.86,4.11,40.55s-28,9.52-40.57-3.41l-52.08-53.6c-18.33,19.5-34.31,36.71-50.52,53.69-12.47,13.07-27.19,16.91-40.53,3.18s-8.6-28.24,4.38-40.55c16.54-15.7,33.06-31.41,56.4-53.59-20.08-17-38.19-31.84-55.64-47.38-14-12.48-19.23-27.9-4.12-42.21,14-13.22,28.46-7.8,40.57,5.12,15.57,16.61,31.06,33.28,50.39,54,20-21.34,35.94-38.68,52.21-55.74,11.81-12.39,26-15.48,38.77-3.31,13.88,13.24,10.45,27.93-2.62,40.51-16.95,16.31-34.17,32.32-55.13,52.1C309.44,275,326.65,290.92,343.72,307Z" />`
+const crossIcon = `    <svg
+xmlns="http://www.w3.org/2000/svg"
+width="512"
+height="512"
+fill="none"
+viewBox="0 0 512 512"
+>
+<path fill="#7D7D7D" d="M94 101H405V382H94z"></path>
+<path
+  fill="#DFDFDF"
+  d="M256 0C114.61 0 0 114.61 0 256s114.61 256 256 256c141.38 0 256-114.61 256-256S397.39 0 256 0zm87.72 307c13.06 12.28 17.61 26.86 4.11 40.55-13.5 13.69-28 9.52-40.57-3.41l-52.08-53.6c-18.33 19.5-34.31 36.71-50.52 53.69-12.47 13.07-27.19 16.91-40.53 3.18s-8.6-28.24 4.38-40.55c16.54-15.7 33.06-31.41 56.4-53.59-20.08-17-38.19-31.84-55.64-47.38-14-12.48-19.23-27.9-4.12-42.21 14-13.22 28.46-7.8 40.57 5.12 15.57 16.61 31.06 33.28 50.39 54 20-21.34 35.94-38.68 52.21-55.74 11.81-12.39 26-15.48 38.77-3.31 13.88 13.24 10.45 27.93-2.62 40.51-16.95 16.31-34.17 32.32-55.13 52.1 20.1 18.64 37.31 34.56 54.38 50.64z"
+></path>
+</svg>`;
 
-module.exports = { crossIcon }
+const deleteIcon = `<svg
+class="delete-icon"
+xmlns="http://www.w3.org/2000/svg"
+x="0"
+y="0"
+viewBox="0 0 7 10.583"
+>
+<g color="#000" fontFamily="sans-serif">
+  <path
+    style={{
+      lineHeight: "normal",
+      fontVariantLigatures: "normal",
+      fontVariantPosition: "normal",
+      fontVariantCaps: "normal",
+      fontVariantNumeric: "normal",
+      fontVariantAlternates: "normal",
+      fontVariantEastAsian: "normal",
+      fontFeatureSettings: "normal",
+      fontVariationSettings: "normal",
+      WebkitTextIndent: "0",
+      textIndent: "0",
+      WebkitTextAlign: "start",
+      textAlign: "start",
+      WebkitTextDecorationLine: "none",
+      textDecorationLine: "none",
+      WebkitTextDecorationStyle: "solid",
+      textDecorationStyle: "solid",
+      WebkitTextDecorationColor: "#000000",
+      textDecorationColor: "#000000",
+      WebkitTextTransform: "none",
+      textTransform: "none",
+      WebkitTextOrientation: "mixed",
+      textOrientation: "mixed",
+      whiteSpace: "normal",
+      shapePadding: "0",
+      shapeMargin: "0",
+      inlineSize: "0",
+      isolation: "auto",
+      mixBlendMode: "normal",
+      solidColor: "#000000",
+      solidOpacity: "1",
+    }}
+    d="M3.506-52.386a.796.796 0 00-.793.792v.264H.799a.265.265 0 100 .529h.474l.315 4.672c.042.619.525 1.15 1.168 1.15H5.71c.643 0 1.125-.531 1.168-1.15l.316-4.672h.473a.265.265 0 100-.53H5.754v-.263a.796.796 0 00-.793-.792zm0 .529H4.96c.151 0 .263.112.263.263v.264H3.24v-.264c0-.15.115-.263.266-.263zM1.8-50.801h4.865l-.313 4.635c-.026.38-.283.658-.641.658H2.756c-.358 0-.615-.279-.64-.658z"
+    enableBackground="accumulate"
+    overflow="visible"
+    transform="translate(0 52.916)"
+  ></path>
+  <path
+    style={{
+      lineHeight: "normal",
+      fontVariantLigatures: "normal",
+      fontVariantPosition: "normal",
+      fontVariantCaps: "normal",
+      fontVariantNumeric: "normal",
+      fontVariantAlternates: "normal",
+      fontVariantEastAsian: "normal",
+      fontFeatureSettings: "normal",
+      fontVariationSettings: "normal",
+      WebkitTextIndent: "0",
+      textIndent: "0",
+      WebkitTextAlign: "start",
+      textAlign: "start",
+      WebkitTextDecorationLine: "none",
+      textDecorationLine: "none",
+      WebkitTextDecorationStyle: "solid",
+      textDecorationStyle: "solid",
+      WebkitTextDecorationColor: "#000000",
+      textDecorationColor: "#000000",
+      WebkitTextTransform: "none",
+      textTransform: "none",
+      WebkitTextOrientation: "mixed",
+      textOrientation: "mixed",
+      whiteSpace: "normal",
+      shapePadding: "0",
+      shapeMargin: "0",
+      inlineSize: "0",
+      isolation: "auto",
+      mixBlendMode: "normal",
+      solidColor: "#000000",
+      solidOpacity: "1",
+    }}
+    d="M3.44-49.742a.265.265 0 00-.264.265v2.645a.265.265 0 00.263.266.265.265 0 00.266-.266v-2.645a.265.265 0 00-.266-.265zM5.027-49.742a.265.265 0 00-.265.265v2.645a.265.265 0 00.265.266.265.265 0 00.264-.266v-2.645a.265.265 0 00-.264-.265z"
+    enableBackground="accumulate"
+    overflow="visible"
+    transform="translate(0 52.916)"
+  ></path>
+</g><svg>`;
+
+const bottomArrowIcon = `<svg
+xmlns="http://www.w3.org/2000/svg"
+x="0"
+y="0"
+enableBackground="new 0 0 48 48"
+viewBox="0 0 48 60"
+>
+<path d="M47.8 7.1C47.4 6.4 46.7 6 46 6H2C1.3 6 .6 6.4.2 7.1s-.3 1.5.1 2.1l22 32c.4.5 1 .9 1.6.9s1.3-.3 1.6-.9l22-32c.6-.7.6-1.5.3-2.1z"></path>
+</svg>`
+
+module.exports = { crossIcon, deleteIcon, bottomArrowIcon };
